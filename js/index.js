27378e64b8940748190367415aa4871f055341ef
mariomosakuchu <mario_mosakuchu@yahoo.co.jp>
2020-08-29 14:49:22 +0900

    animate.cssで冨居気を付与

---
+++ js/index.js
@@ -0,0 +1,36 @@
+jQuery(function() {
+  var appear = false;
+  var pagetop = $('#page_top');
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {  //100pxスクロールしたら
+      if (appear == false) {
+        appear = true;
+        pagetop.stop().animate({
+          'bottom': '50px' //下から50pxの位置に
+        }, 300); //0.3秒かけて現れる
+      }
+    } else {
+      if (appear) {
+        appear = false;
+        pagetop.stop().animate({
+          'bottom': '-50px' //下から-50pxの位置に
+        }, 300); //0.3秒かけて隠れる
+      }
+    }
+  });
+  pagetop.click(function () {
+    $('body, html').animate({ scrollTop: 0 }, 1000); //1秒かけてトップへ戻る
+    return false;
+  });
+
+  $(window).scroll(function (){
+      if($(this).scrollTop() > 400) {
+        $('.flex.wrapper').addClass('animated fadeInUp');
+      }else{
+          $('.flex.wrapper').removeClass('animated fadeInUp');
+      }
+  });
+
+
+
+});
